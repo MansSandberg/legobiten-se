0f21ec42c469578e5e7af0a2aefe5d4f6a276cfd
Måns Sandberg <m.sandberg@me.com>
2026-08-03 20:46:24 +0200

    Skapar komponenten Taglist

---
+++ src/components/TagList.tsx
@@ -0,0 +1,21 @@
+
+type TagListProps = {
+  tags: string[]
+}
+
+function TagList({tags}: TagListProps) {
+  if (!tags || tags.length === 0) return null
+
+  return (
+    <>
+      <div className="tags">
+        {tags.map((tag) => (
+          <span key={tag} className="tag">{tag}</span>
+        ))}
+      </div>
+    </>
+  )
+}
+
+
+export default TagList;
